e8b0c2fc040c8f953fa56263ca1b7c9d9dfa6393
LugeK1t <263635591@qq.com>
2026-01-16 22:29:36 +0800

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <main className="m-0 pt-[10vh] flex flex-col justify-center text-center">
+      <div>
+        <button onClick={() => {
+        }}>
+          Cut Me!
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
