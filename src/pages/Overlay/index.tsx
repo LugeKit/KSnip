1c3c59427626dbaa500b69a8ba6f8d453e382593
LugeK1t <263635591@qq.com>
2026-01-17 21:00:32 +0800

    feat: drag to paint a crop area

--- src/pages/Overlay/index.tsx
+++ src/pages/Overlay/index.tsx
@@ -1,23 +1,95 @@
 import { getCurrentWindow } from "@tauri-apps/api/window";
-import { error, info } from "@tauri-apps/plugin-log";
+import { info } from "@tauri-apps/plugin-log";
+import React, { useCallback, useEffect, useState } from "react";
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+interface Rectangle {
+    left: number;
+    top: number;
+    width: number;
+    height: number;
+}
+
+const screenshotThresholdPixel = 5;
 
 export default function OverlayPage() {
-  info(`[OverlayPage] OverlayPage loaded`);
-  const appWindow = getCurrentWindow();
+    info(`[OverlayPage] OverlayPage loaded`);
+    const [startPosition, setStartPosition] = useState<Point | null>(null);
+    const [cropArea, setCropArea] = useState<Rectangle | null>(null);
+    const [isDragging, setIsDragging] = useState(false);
 
-  return (
-    <div className="flex justify-center items-center bg-transparent w-screen h-screen">
-      <button
-        className="bg-blue-300 w-100 h-10 text-black"
-        onClick={(_) => {
-          info(`[OverlayPage] button clicked`);
-          appWindow.close().catch((e) => {
-            error(`[OverlayPage] close error: ${e}`);
-          });
-        }}
-      >
-        click me again
-      </button>
-    </div>
-  );
+    // closeOverlayPage closes the overlay page
+    const closeOverlayPage = useCallback(() => {
+        const appWindow = getCurrentWindow();
+        appWindow.close();
+    }, []);
+
+    // register global keydown event listener to close overlay page
+    // press "Esc" to close the overlay page
+    useEffect(() => {
+        const handleGlobalKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                closeOverlayPage();
+            }
+        };
+        window.addEventListener("keydown", handleGlobalKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleGlobalKeyDown);
+        };
+    }, [closeOverlayPage]);
+
+    const handleMouseDown = (e: React.MouseEvent) => {
+        info(`[OverlayPage] handleMouseDown: ${e.clientX}, ${e.clientY}`);
+        setStartPosition({ x: e.clientX, y: e.clientY });
+        setIsDragging(true);
+    };
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+        if (!isDragging || !startPosition) {
+            return;
+        }
+        info(`[OverlayPage] handleMouseMove: ${e.clientX}, ${e.clientY}`);
+
+        const currentPosition = { x: e.clientX, y: e.clientY };
+        const width = Math.abs(startPosition.x - currentPosition.x);
+        const height = Math.abs(startPosition.y - currentPosition.y);
+        if (
+            width <= screenshotThresholdPixel ||
+            height <= screenshotThresholdPixel
+        ) {
+            return;
+        }
+
+        setCropArea({
+            left: Math.min(startPosition.x, currentPosition.x),
+            top: Math.min(startPosition.y, currentPosition.y),
+            width,
+            height,
+        });
+    };
+
+    const handleMouseUp = (e: React.MouseEvent) => {
+        info(`[OverlayPage] handleMouseUp: ${e.clientX}, ${e.clientY}`);
+        setIsDragging(false);
+    };
+
+    return (
+        <div
+            className="fixed bg-gray-800/20 w-screen h-screen"
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+        >
+            {cropArea && (
+                <div
+                    className="absolute border-2 border-red-500 bg-transparent"
+                    style={{ ...cropArea }}
+                />
+            )}
+        </div>
+    );
 }
