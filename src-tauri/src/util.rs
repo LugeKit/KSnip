12db6c9107bfc059642d2d544cd3f9d41670ff08
LugeK1t <263635591@qq.com>
2026-01-18 11:11:35 +0800

    style: clean unused code

--- src-tauri/src/util.rs
+++ src-tauri/src/util.rs
@@ -2,12 +2,6 @@ use std::borrow::Cow;
 
 use arboard::{Clipboard, ImageData};
 
-pub fn save_text_to_clipboard<'a, S: Into<&'a str>>(text: S) -> Result<(), String> {
-    let mut clipboard = Clipboard::new().map_err(|e| e.to_string())?;
-    clipboard.set_text(text.into()).map_err(|e| e.to_string())?;
-    Ok(())
-}
-
 pub fn save_image_to_clipboard(width: usize, height: usize, image: Vec<u8>) -> Result<(), String> {
     let image_data = ImageData {
         width,
