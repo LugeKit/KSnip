0b4d8bea6a83d3083c3b01182b0903f62045ed21
LugeK1t <263635591@qq.com>
2026-01-18 13:08:58 +0800

    style: move start snip out of MainPage

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -2,32 +2,13 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { registerGlobalShortcut } from "@/lib/utils";
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { debug, error, info } from "@tauri-apps/plugin-log";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import AppSidebar from "./components/AppSideBar";
 
 export default function MainPage() {
-    const startCut = useCallback(() => {
-        debug("[MainPage] start cut");
-        const overlayWindow = new WebviewWindow("screenshot-overlay", {
-            url: "index.html#screenshot",
-            fullscreen: true,
-            transparent: true,
-            alwaysOnTop: true,
-            decorations: false,
-        });
-
-        overlayWindow.once("tauri://created", () => {
-            info("[MainPage] overlayWindow created");
-        });
-
-        overlayWindow.once("tauri://error", (e) => {
-            error(`[MainPage] overlayWindow error: ${e.payload}`);
-        });
-    }, []);
-
     useEffect(() => {
-        registerGlobalShortcut("Alt+A", startCut);
-    }, [startCut]);
+        registerGlobalShortcut("Alt+A", startSnip);
+    }, [startSnip]);
 
     return (
         <SidebarProvider>
@@ -37,4 +18,23 @@ export default function MainPage() {
             </main>
         </SidebarProvider>
     );
+}
+
+function startSnip() {
+    debug("[MainPage] start cut");
+    const overlayWindow = new WebviewWindow("screenshot-overlay", {
+        url: "index.html#screenshot",
+        fullscreen: true,
+        transparent: true,
+        alwaysOnTop: true,
+        decorations: false,
+    });
+
+    overlayWindow.once("tauri://created", () => {
+        info("[MainPage] overlayWindow created");
+    });
+
+    overlayWindow.once("tauri://error", (e) => {
+        error(`[MainPage] overlayWindow error: ${e.payload}`);
+    });
 }
