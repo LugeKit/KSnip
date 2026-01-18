199a1ce0854d2b19c3d745cd4da7cded4a8b9745
LugeK1t <263635591@qq.com>
2026-01-18 21:32:56 +0800

    feat: finish ui adjustment for shortcut settings

--- src/pages/Main/components/ShortcutSettings.tsx
+++ src/pages/Main/components/ShortcutSettings.tsx
@@ -9,6 +9,8 @@ import {
     TableRow,
 } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { debug } from "@tauri-apps/plugin-log";
+import { useEffect, useState } from "react";
 
 export default function ShortcutSettings() {
     const tabs = [
@@ -35,31 +37,49 @@ export default function ShortcutSettings() {
         },
     ];
 
+    const tabsState = tabs.map((tab) => {
+        return {
+            ...tab,
+            shortcuts: tab.shortcuts.map((shortcut) => {
+                const [enabled, setEnabled] = useState(shortcut.enabled);
+
+                useEffect(() => {
+                    debug(
+                        `[ShortcutSettings] command: ${shortcut.command}, enabled: ${enabled}`,
+                    );
+                }, [enabled]);
+
+                return {
+                    ...shortcut,
+                    enabled,
+                    setEnabled,
+                };
+            }),
+        };
+    });
+
     return (
         <div className="relative top-0 right-0 w-full h-full p-4">
             <Tabs defaultValue="basic" className="w-full">
                 <TabsList>
-                    {tabs.map((tab) => (
+                    {tabsState.map((tab) => (
                         <TabsTrigger key={tab.value} value={tab.value}>
                             {tab.label}
                         </TabsTrigger>
                     ))}
                 </TabsList>
                 <div className="w-full border-border border-b left-0 mt-2 mb-2" />
-                {tabs.map((tab) => (
+                {tabsState.map((tab) => (
                     <TabsContent key={tab.value} value={tab.value}>
                         <div className="[&_tr]:hover:bg-transparent bg-muted rounded-md pl-5 pr-5">
                             <Table>
                                 <TableHeader>
                                     <TableRow>
-                                        <TableHead className="font-bold w-3/4">
+                                        <TableHead className="font-bold w-2/3 flex-1">
                                             功能
                                         </TableHead>
-                                        <TableHead className="font-bold w-1/4 whitespace-nowrap">
+                                        <TableHead className="font-bold whitespace-nowrap">
                                             快捷键
-                                        </TableHead>
-                                        <TableHead className="font-bold w-1/4 text-right">
-                                            启用
                                         </TableHead>
                                     </TableRow>
                                 </TableHeader>
@@ -70,17 +90,30 @@ export default function ShortcutSettings() {
                                                 {shortcut.command}
                                             </TableCell>
                                             <TableCell>
-                                                <KbdGroup>
+                                                <KbdGroup className="hover:bg-muted-foreground p-1 rounded-md">
                                                     {shortcut.shortcut.map(
                                                         (key) => (
-                                                            <Kbd>{key}</Kbd>
+                                                            <Kbd className="bg-white">
+                                                                {key}
+                                                            </Kbd>
                                                         ),
                                                     )}
                                                 </KbdGroup>
                                             </TableCell>
-                                            <TableCell>
+                                            <TableCell className="text-right">
                                                 <Checkbox
+                                                    className="data-[state=checked]:bg-transparent data-[state=checked]:text-black mr-1"
                                                     checked={shortcut.enabled}
+                                                    onCheckedChange={(
+                                                        checked,
+                                                    ) =>
+                                                        shortcut.setEnabled(
+                                                            checked ===
+                                                                "indeterminate"
+                                                                ? true
+                                                                : checked,
+                                                        )
+                                                    }
                                                 />
                                             </TableCell>
                                         </TableRow>
