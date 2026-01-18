c31e780da7581fa6674244cd67889cb788bb74a6
LugeK1t <263635591@qq.com>
2026-01-18 19:37:21 +0800

    feat: add tabs in shortcut settings

--- src/pages/Main/components/ShortcutSettings.tsx
+++ src/pages/Main/components/ShortcutSettings.tsx
@@ -1,5 +1,15 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function ShortcutSettings() {
     return (
-        <h1>Shortcut Settings</h1>
+        <div className="relative top-0 right-0 w-full h-full">
+            <Tabs defaultValue="basic" className="w-full m-4">
+                <TabsList className="gap-6">
+                    <TabsTrigger value="basic">基础</TabsTrigger>
+                    <TabsTrigger value="screenshot">截图界面</TabsTrigger>
+                </TabsList>
+            </Tabs>
+            <div className="w-full border-border border-b left-0" />
+        </div>
     );
 }
