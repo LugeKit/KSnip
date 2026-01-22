35cffc2d8b4eb886867b1cd5e5b0cf22d7105908
LugeK1t <263635591@qq.com>
2026-01-22 23:11:35 +0800

    feat: add position for overlay page

--- src/pages/Overlay/OverlayPage.tsx
+++ src/pages/Overlay/OverlayPage.tsx
@@ -55,7 +55,7 @@ export default function OverlayPage() {
 
     return (
         <div
-            className="fixed bg-transparent w-screen h-screen"
+            className="fixed top-0 left-0 bg-transparent w-screen h-screen"
             onMouseDown={handleMouseDown}
             onMouseMove={handleMouseMove}
             onMouseUp={handleMouseUp}
