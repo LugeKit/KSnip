6138fce0137596362c149e9d3182b73e5b679c16
LugeK1t <263635591@qq.com>
2026-01-23 22:22:27 +0800

    feat: get id from url

--- src-tauri/src/protocol.rs
+++ src-tauri/src/protocol.rs
@@ -3,18 +3,52 @@ use tauri::{
     Runtime, UriSchemeContext,
 };
 use tauri_plugin_log::log::info;
+use url::form_urlencoded::{self};
 
 pub fn handle<R: Runtime>(
     _ctx: UriSchemeContext<'_, R>,
     request: Request<Vec<u8>>,
 ) -> Response<Vec<u8>> {
-    let path = request.uri().path();
     let response_builder = Response::builder().header("Access-Control-Allow-Origin", "*");
+    match internal_handle(_ctx, request) {
+        Ok(body) => response_builder.status(StatusCode::OK).body(body).unwrap(),
+        Err(e) => response_builder
+            .status(StatusCode::BAD_REQUEST)
+            .body(e.into_bytes())
+            .unwrap(),
+    }
+}
 
-    info!("[protocol] path: {}", path);
+fn internal_handle<R: Runtime>(
+    _ctx: UriSchemeContext<'_, R>,
+    request: Request<Vec<u8>>,
+) -> Result<Vec<u8>, String> {
+    let uri = request.uri();
+    let raw_path = uri.path();
+    let decoded_path = percent_encoding::percent_decode_str(raw_path).decode_utf8_lossy();
 
-    response_builder
-        .status(StatusCode::OK)
-        .body(Vec::new())
-        .unwrap()
+    let (_, query) = match decoded_path.split_once('?') {
+        Some((path, query)) => (path, Some(query)),
+        None => (raw_path, uri.query()),
+    };
+
+    info!(
+        "[protocol] uri: {}, path: {}, query: {:?}, decoded_path: {}, raw_query: {:?}",
+        uri,
+        raw_path,
+        query,
+        decoded_path,
+        uri.query()
+    );
+
+    let query = query.ok_or(String::from("query is empty"))?;
+    let mut parsed = form_urlencoded::parse(query.as_bytes());
+    let id = parsed
+        .find(|(k, _)| k == "id")
+        .map(|(_, v)| v.into_owned())
+        .ok_or(String::from("id is empty"))?;
+
+    info!("[protocol] id: {}", id);
+
+    Ok(Vec::new())
 }
--- src/pages/Pin/PinPage.tsx
+++ src/pages/Pin/PinPage.tsx
@@ -22,15 +22,11 @@ export default function PinPage() {
 
     return (
         <div className="left-0 top-0 w-screen h-screen bg-white">
-            {pinID > 0 && <img src={convertFileSrc(`pin/id=${pinID}`, "ksnip")} />}
+            {pinID > 0 && <img src={convertFileSrc(`pin?id=${pinID}`, "ksnip")} />}
             <div
                 className="fixed top-0 left-0 w-full h-full bg-black z-1"
                 data-tauri-drag-region
-                onDoubleClick={() => {
-                    debug(`[PinPage] double click`);
-                    const app = getCurrentWindow();
-                    app.close();
-                }}
+                onDoubleClick={() => getCurrentWindow().close()}
                 onWheel={(e: React.WheelEvent) => {
                     debug(`[PinPage] wheel: ${e.deltaY}`);
                 }}
