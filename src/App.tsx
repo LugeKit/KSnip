038a068aaec71237ea2eb026da7095287b24d115
LugeK1t <263635591@qq.com>
2026-01-23 21:25:59 +0800

    style: rename PinPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { info } from "@tauri-apps/plugin-log";
 import MainPage from "./pages/Main/MainPage";
 import OverlayPage from "./pages/Overlay/OverlayPage";
-import PinPage from "./pages/Pin/pin";
+import PinPage from "./pages/Pin/PinPage";
 
 export default function App() {
     const hash = window.location.hash;
