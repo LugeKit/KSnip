37a597d5e9275ec677f683ef47fa28b3480f63f2
LugeK1t <263635591@qq.com>
2026-01-30 21:54:15 +0800

    feat: use debug setting in debug button

--- src/components/ui/AppHeader.tsx
+++ src/components/ui/AppHeader.tsx
@@ -1,28 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { ButtonGroup } from "@/components/ui/button-group";
+import { ENABLE_DEBUG_SETTING } from "@/services/setting/const";
+import { getSetting } from "@/services/setting/setting";
+import { SettingValueBoolean } from "@/services/setting/types";
 import { getLocalStore } from "@/services/store";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import { error } from "@tauri-apps/plugin-log";
 import { Heart, Minus, RectangleEllipsis, X } from "lucide-react";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function AppHeader() {
     const appWindow = useMemo(() => getCurrentWindow(), []);
+    const [isDebug, setIsDebug] = useState(false);
+    useEffect(() => {
+        getSetting(ENABLE_DEBUG_SETTING).then((setting) => {
+            if (!setting) {
+                return;
+            }
+
+            setIsDebug((setting.value as SettingValueBoolean).value);
+        });
+    }, []);
 
     return (
         <div className="relative top-0 left-0 h-(--header-height) w-full flex justify-end bg-sidebar">
             <div className="absolute left-0 top-0 w-full h-full border-b border-border" />
             <div className="absolute inset-0" data-tauri-drag-region onDoubleClick={() => appWindow.toggleMaximize()} />
             <ButtonGroup className="relative z-1 h-full [&>button]:aspect-square [&>button]:h-full">
-                <Button
-                    variant={"ghost"}
-                    onClick={async () => {
-                        const store = await getLocalStore();
-                        store.clear();
-                    }}
-                >
-                    <Heart />
-                </Button>
+                {isDebug && (
+                    <Button
+                        variant={"ghost"}
+                        onClick={async () => {
+                            const store = await getLocalStore();
+                            store.clear();
+                        }}
+                    >
+                        <Heart />
+                    </Button>
+                )}
                 <Button variant={"ghost"} onClick={() => appWindow.minimize()}>
                     <Minus />
                 </Button>
